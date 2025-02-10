b55359095bd9256901bb5be4742de3a142586282
Eli <148476551+zenelionn@users.noreply.github.com>
2025-02-10 17:24:29 +0000

    tutorial finished but code is broken go ask help!

--- 3D website Template_MASTER/3D website Template_MASTER/script.js
+++ 3D website Template_MASTER/3D website Template_MASTER/script.js
@@ -1,59 +1,87 @@
-var scene, camera, renderer, box;
+var scene, camera, renderer, clock, mixer, action = [], mode;
 
 init();
 
 function init(){
-  
-scene = new THREE.Scene();
- 
 
- scene.background = new THREE.Color(0xaaaaaa);
+    const assetPath = './';
+
+    clock = new THREE.Clock();
+  
+    scene = new THREE.Scene();
+ 
+    scene.background = new THREE.Color(0xaaaaaa);
   
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 3;
-
-
-
-
-
-
+camera.position.set(-5,25,20);
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
-
-
-
   document.body.appendChild( renderer.domElement );
+
+  const ambient = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+  scene.add(ambient);
+
   
-  const light = new THREE.DirectionalLight();
-  light.position.set(0, 1, 2);
+  const light = new THREE.DirectionalLight(0xFFFFFF);
+  light.position.set(0, 10, 2);
   scene.add(light);
 
+  const controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.target.set(1, 2, 0);
+  controls.update();
+
+  // Button to control animations
+  mode = 'open';
+  const btn = document.getElementById("btn");
+  btn.addEventListener('click', function() {
+    if (actions.length === 2) {
+      if (mode === "open") {
+        actions.forEach(action => {
+          action.timeScale = 1;
+          action.reset();
+          action.play();
+        });
+      }
+    }
+  });
+
+    
+ // Load the glTF model
+  const loader = new THREE.GLTFLoader();
+  loader.load(assetPath + 'web3Dmodel.glb', function(gltf) {
+    const model = gltf.scene;
+    scene.add(model);
+    
+    // Set up animations 
+    mixer = new THREE.AnimationMixer(model);
+    const animations = gltf.animations; 
+
+    animations.forEach(clip => {
+      const action = mixer.clipAction(clip);
+      actions.push(action);
+    });
+
+  });
 
 
-
-
-  
-  const material = new THREE.MeshStandardMaterial({color: new THREE.Color('skyblue')});
- 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  box = new THREE.Mesh(geometry, material);
-  box.position.x = 0;
-  scene.add(box);
 
   window.addEventListener('resize', onResize, false);
   
-  update();
+  animate();
 }
 
 
-function update(){
-  requestAnimationFrame(update);
-  
-  box.rotation.y += 0.01;
-  
-  // Render the scene
-  renderer.render(scene, camera);
+function animate(){
+    requestAnimationFrame(animate);
+    if (mixer){
+        mixer.update(clock.getDelta());
+    }
+
+    
+    renderer.render(scene, camera);
 }
+
+
 
 function onResize(){
   camera.aspect = window.innerWidth / window.innerHeight;
