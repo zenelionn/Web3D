747aa4b4c9ad913768c45d83948a6c6a4524a62f
Eli <148476551+zenelionn@users.noreply.github.com>
2025-03-18 18:47:38 +0000

    eepiest guy in the world im done for today dont think i added anything new

--- 3D website Template_MASTER/3D website Template_MASTER/script2.js
+++ 3D website Template_MASTER/3D website Template_MASTER/script2.js
@@ -12,11 +12,13 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0x9fabb5);
 
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000);
+
   camera.position.set(-5, 5, 20);
 
   
   renderer = new THREE.WebGLRenderer();
+  
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
